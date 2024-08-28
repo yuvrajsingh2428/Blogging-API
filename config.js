715583cb72df94621bc1dc330a18ad76e661f8bf
yuvrajsingh2428 <offcyuvi2428@gmail.com>
2024-08-29 01:21:26 +0530

    Added controllers

---
+++ config.js
@@ -0,0 +1,13 @@
+require("dotenv").config()
+
+const DATABASE_CONNECT_STRING = process.env.DATABASE_CONNECT_STRING;
+
+const PORT = 8080 || process.env.PORT
+
+const SECRET = process.env.SECRET;
+
+module.exports = {
+    DATABASE_CONNECT_STRING,
+    PORT,
+    SECRET
+}
